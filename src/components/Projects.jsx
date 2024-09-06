2964db376b10458229a5bb401aba18816f75bb37
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-09-06 19:22:09 -0300

    actualizando portfolio con proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,78 @@
 import React from 'react';
+import intro from '../assets/MacBook Pro-1725659071559.jpeg';
+import dashboard from '../assets/MacBook Pro-1725660189977.jpeg';
+import news from '../assets/MacBook Pro-1725637254155.jpeg';
 
 const Projects = () => (
   <div className="flex flex-col items-center px-4">
+
+<div className="mt-8 text-center">
+      <p className="font-bold text-2xl"> 
+       News homepage
+      </p>
+      <p className="mb-4">
+      Creé una página de inicio de un sitio web de noticias con un diseño responsivo, replicando el diseño proporcionado, tanto para pantallas de escritorio como para dispositivos móviles y tablets, utilizando HTML, JavaScript, React Vite y Tailwind CSS. Los principales elementos que debíann implementarse incluían:
+      Diseño de varias secciones: La página incluye una cabecera con un menú de navegación desplegable (en la vista móvil), una sección destacada con una imagen principal y un título, una lista de artículos recientes, y una barra lateral con noticias adicionales.
+      Diseño responsivo: El desafío incluye un diseño que se adapte tanto a pantallas de escritorio como a dispositivos móviles y tablets. En la vista móvil, el menú de navegación esaccesible a través de un ícono de hamburguesa que despliega las opciones.
+      Interactividad: En la versión móvil, el menú se despliega y cierra. También se agregó interactividad extra como efectos al hacer hover o clic en las noticias.
+      </p>
+      <div className="mt-8">
+        <a href="https://www.loom.com/share/ea229d50262e4e349291e1eabd5470a1?sid=5a7c76a3-2c16-4b43-8d92-a900673e8318" target="_blank" rel="noopener noreferrer">
+          <img
+            src={news}
+            alt="Descripción del video"
+            className="w-48 h-auto transition-transform transform hover:scale-105 mx-auto"
+          />
+        </a>
+      </div>
+    </div>
+
+
+  <div className="mt-8 text-center">
+      <p className="font-bold text-2xl"> 
+      Social media dashboard with theme switcher
+      </p>
+      <p className="mb-4">
+      Este proyecto consiste en una página de destino que simula un panel de control de redes sociales. Incluye un conmutador de temas de color que permite a los usuarios cambiar entre un tema claro y uno oscuro. El diseño responde de manera óptima a diferentes tamaños de pantalla y presenta estados de hover y focus para todos los elementos interactivos. Se utilizaron las siguientes tecnologías HTML, JavaScript, React Vite y Tailwind.
+      Funcionalidades
+      Alternar el tema de color según la preferencia del usuario.
+      Visualizar un diseño óptimo de la interfaz según el tamaño de la pantalla del dispositivo.
+      Ver estados de hover y focus para todos los elementos interactivos.
+
+      </p>
+      <div className="mt-8">
+        <a href="https://www.loom.com/share/6d044fb6d7dc43099ec208722129a5f6?sid=ec153fea-3876-4f05-83a3-62b01fbbc146" target="_blank" rel="noopener noreferrer">
+          <img
+            src={dashboard}
+            alt="Descripción del video"
+            className="w-48 h-auto transition-transform transform hover:scale-105 mx-auto"
+          />
+        </a>
+      </div>
+    </div>
+
+
+  <div className="mt-8 text-center">
+      <p className="font-bold text-2xl"> 
+      Sección de introducción con navegación desplegable
+      </p>
+      <p className="mb-4">
+      Este proyecto es una implementación de una sección de introducción con navegación desplegable, diseñada para proporcionar una experiencia de usuario óptima tanto en dispositivos móviles como en escritorio. El proyecto fue desarrollado utilizando HTML, CSS y JavaScript.
+      Funcionalidades implementadas
+      Menús desplegables interactivos: Los menús de navegación se despliegan al hacer clic en los enlaces correspondientes, tanto en dispositivos móviles como en escritorio.
+      Diseño responsivo: El contenido de la página se ajusta automáticamente para proporcionar un diseño óptimo en función del tamaño de la pantalla del dispositivo.
+            </p>
+      <div className="mt-8">
+        <a href="https://www.loom.com/share/ea229d50262e4e349291e1eabd5470a1?sid=210b37a2-3686-49ac-a692-0dbdc93e5c86" target="_blank" rel="noopener noreferrer">
+          <img
+            src={intro}
+            alt="Descripción del video"
+            className="w-48 h-auto transition-transform transform hover:scale-105 mx-auto"
+          />
+        </a>
+      </div>
+    </div>
+
     <div className="mt-8 text-center">
       <p className="font-bold text-2xl"> 
         Creación de una E-commerce de indumentaria deportiva - Pinkpanther
